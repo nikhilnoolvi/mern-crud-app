231a5c5242dd9e2b9fe40b20ab1562854a5378f0
noolvinikhil24@gmail.com <noolvinikhil24@gmail.com>
2025-11-14 10:41:29 +0530

    jest supertest is added

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,5 @@
-const express = require("express");
-const mongoose = require("mongoose"); //
-const cors = require("cors");
-const dotenv = require("dotenv");
-const userRoutes = require("./routes/userRoutes");
-const swaggerUi = require("swagger-ui-express");
-const swaggerSpec = require("./swagger");
 
-dotenv.config();
-
-const app = express();
-app.use(cors());
-app.use(express.json());
-
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB connected successfully"))
-  .catch((err) => console.error("MongoDB connection error:", err));
-
-app.use("/api/users", userRoutes);
-// Swagger Documentation route
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+const app = require("./app");
 
 const PORT = process.env.PORT;
 //app.listen(PORT, () => console.log(` Server running on port ${PORT}`));
