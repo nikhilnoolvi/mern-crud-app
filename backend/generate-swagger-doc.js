b6e0700e18e0dd375e55ff8816758f830c43fb63
noolvinikhil24@gmail.com <noolvinikhil24@gmail.com>
2025-11-18 23:01:36 +0530

    update backend swagger

--- backend/generate-swagger-doc.js
+++ backend/generate-swagger-doc.js
@@ -1,11 +1,11 @@
 const fs = require("fs");
 const path = require("path");
 const swaggerUiDist = require("swagger-ui-dist");
-const swaggerSpec = require("./swagger"); // your swagger-jsdoc
+const swaggerSpec = require("./swagger"); // your swagger-jsdoc config
 
 const outputDir = path.join(__dirname, "../docs");
 
-// 1. Create /docs folder
+// 1. Create docs folder if it doesn't exist
 if (!fs.existsSync(outputDir)) {
   fs.mkdirSync(outputDir);
 }
@@ -14,14 +14,16 @@ if (!fs.existsSync(outputDir)) {
 const swaggerDistPath = swaggerUiDist.getAbsoluteFSPath();
 fs.cpSync(swaggerDistPath, outputDir, { recursive: true });
 
-// 3. Save swagger.json
-const swaggerJsonPath = path.join(outputDir, "swagger.json");
-fs.writeFileSync(swaggerJsonPath, JSON.stringify(swaggerSpec, null, 2));
+// 3. Write swagger.json
+fs.writeFileSync(path.join(outputDir, "swagger.json"), JSON.stringify(swaggerSpec, null, 2));
 
-// 4. Replace index.html to load our swagger.json
+// 4. Update index.html to load local swagger.json
 const indexHtmlPath = path.join(outputDir, "index.html");
 let indexHtml = fs.readFileSync(indexHtmlPath, "utf8");
-indexHtml = indexHtml.replace("https://petstore.swagger.io/v2/swagger.json", "swagger.json");
+indexHtml = indexHtml.replace(
+  "https://petstore.swagger.io/v2/swagger.json",
+  "swagger.json"
+);
 fs.writeFileSync(indexHtmlPath, indexHtml);
 
 console.log("Swagger documentation generated successfully!");
